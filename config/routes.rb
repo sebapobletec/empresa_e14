848a9797182d3469c4c1e824ef4601cb8db8b797
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-05 00:08:18 -0400

    Add Events scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :companies do
     resources :employees, only: [:create, :destroy]
   end
-  root 'companies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
